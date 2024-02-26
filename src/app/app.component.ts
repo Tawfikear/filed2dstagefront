bfbc8d56cd584dab19f1eb146c1ffcbf24a7b87d
tawfik akaridan <tawfikear@gmail.com>
2024-02-26 16:55:49 +0100

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+
+  formulaire = new FormGroup({
+    email: new FormControl(''),
+    expiration: new FormControl(''),
+
+  })
+
+  fichierAUpload: File[] = [];
+
+
+  onFilesSelected(event: any) {
+    const files = event.target.files;
+    for(const file of files) {
+      this.fichierAUpload.push(file)
+    }
+  }
+
+
+  onSubmit() {
+
+    const formData = new FormData();
+    
+  }
+
 }
+
+
